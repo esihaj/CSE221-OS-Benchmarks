b69784a746398c17553fb6a03b09cfd6d2e167bd
Ehsan Hajyasini <esihaj@gmail.com>
2022-11-20 16:28:25 -0800

    update size in page fault

--- memory/page-fault/pagefault.cpp
+++ memory/page-fault/pagefault.cpp
@@ -4,24 +4,31 @@
 #include <cstdlib>
 #include <cassert>
 #include <set>
+#include <limits>
 
 using namespace std;
 
+const int BLOCK_SIZE = 4096;
 const int ACCESS_COUNT = 10000;
-const int SIZE = 4'000'000;
+const int SIZE = INT_MAX / BLOCK_SIZE;
 const int STRIDE_LIMIT = (SIZE - 1) / ACCESS_COUNT;
 
-int main() {
+int main()
+{
     std::srand(42);
-    char * file_address = "/home/ehsan/rand-large.bin";
+    cout << "selected MAPPING BLOCK COUNT " << SIZE << endl;
+    
+    char *file_address = "/home/ehsan/rand-large.bin";
     int file_read = open(file_address, O_RDONLY, 0);
-    if (file_read == -1) {
+    if (file_read == -1)
+    {
         cerr << "could not open file " << file_address << endl;
         exit(1);
     }
-    char *buffer = reinterpret_cast<char*>(
-        mmap(nullptr, 4096*SIZE, PROT_READ, MAP_SHARED, file_read, 0));
-    if (buffer == MAP_FAILED) {
+    char *buffer = reinterpret_cast<char *>(
+        mmap(nullptr, BLOCK_SIZE * SIZE, PROT_READ, MAP_PRIVATE, file_read, 0));
+    if (buffer == MAP_FAILED)
+    {
         cerr << "could not map";
         exit(1);
     }
@@ -29,12 +36,12 @@ int main() {
     unsigned long counter = 0;
 
     size_t current_index = 0;
-
-    for(int i = 0; i < ACCESS_COUNT; i++) {
-        assert(STRIDE_LIMIT - 10 > 0);
-        size_t index_page = rand() % (STRIDE_LIMIT-10) + 10; // each access is at least 10 4KB blocks away
+    assert(STRIDE_LIMIT - 10 > 0);
+    for (int i = 0; i < ACCESS_COUNT; i++)
+    {  
+        size_t index_page = rand() % (STRIDE_LIMIT - 10) + 10; // each access is at least 10 4KB blocks away
         current_index += index_page;
-        counter += buffer[current_index * 4096];    
+        counter += buffer[current_index * BLOCK_SIZE];
     };
     cout << "don't optimize away " << counter << endl;
 }
