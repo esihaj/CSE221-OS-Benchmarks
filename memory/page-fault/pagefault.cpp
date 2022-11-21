11352f983e924d02f8c9fcef20f93817b66ebd4a
Ehsan Hajyasini <esihaj@gmail.com>
2022-11-20 16:05:18 -0800

    remove duplicate access from pagefaults

--- memory/page-fault/pagefault.cpp
+++ memory/page-fault/pagefault.cpp
@@ -2,10 +2,11 @@
 #include <sys/mman.h>
 #include <iostream>
 #include <cstdlib>
+#include <set>
 
 using namespace std;
 
-const int ACCESS_COUNT = 1000;
+const int ACCESS_COUNT = 10000;
 const int SIZE = 200'000;
 
 int main() {
@@ -24,11 +25,20 @@ int main() {
 
     unsigned long counter = 0;
 
+    std::set<int> access_history;
+    int loop_redo_counter = 0;
+
     for(int i = 0; i < ACCESS_COUNT; i++) {
-        size_t index_page = rand() % SIZE;
+        size_t index_page;
+        do {
+            index_page = rand() % SIZE;
+            loop_redo_counter++;
+        } while (access_history.find(index_page) != access_history.end());
+
+        access_history.insert(index_page);
         // cout << "accessing page " << index_page << endl;
         counter += buffer[index_page * 4096];    
     };
-
+    cout << "redo count " << loop_redo_counter - ACCESS_COUNT << endl;
     cout << "don't optimize away " << counter << endl;
 }
