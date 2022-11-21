79f848c95b69dde8504034b07675f8492008aa5d
Ehsan Hajyasini <esihaj@gmail.com>
2022-11-20 16:17:53 -0800

    use stride based steps for pagefaults

--- memory/page-fault/pagefault.cpp
+++ memory/page-fault/pagefault.cpp
@@ -2,12 +2,14 @@
 #include <sys/mman.h>
 #include <iostream>
 #include <cstdlib>
+#include <cassert>
 #include <set>
 
 using namespace std;
 
 const int ACCESS_COUNT = 10000;
-const int SIZE = 200'000;
+const int SIZE = 4'000'000;
+const int STRIDE_LIMIT = (SIZE - 1) / ACCESS_COUNT;
 
 int main() {
     std::srand(42);
@@ -26,20 +28,13 @@ int main() {
 
     unsigned long counter = 0;
 
-    std::set<int> access_history;
-    int loop_redo_counter = 0;
+    size_t current_index = 0;
 
     for(int i = 0; i < ACCESS_COUNT; i++) {
-        size_t index_page;
-        do {
-            index_page = rand() % SIZE;
-            loop_redo_counter++;
-        } while (access_history.find(index_page) != access_history.end());
-
-        access_history.insert(index_page);
-        // cout << "accessing page " << index_page << endl;
-        counter += buffer[index_page * 4096];    
+        assert(STRIDE_LIMIT - 10 > 0);
+        size_t index_page = rand() % (STRIDE_LIMIT-10) + 10; // each access is at least 10 4KB blocks away
+        current_index += index_page;
+        counter += buffer[current_index * 4096];    
     };
-    cout << "redo count " << loop_redo_counter - ACCESS_COUNT << endl;
     cout << "don't optimize away " << counter << endl;
 }
