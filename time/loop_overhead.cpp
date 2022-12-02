cf885afe2800764123296ac9c1312006e52cd955
Ehsan Hajyasini <esihaj@gmail.com>
2022-12-01 22:46:03 -0800

    add loop overhead

---
+++ time/loop_overhead.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "rdtscp_timer.h"
+using namespace std;
+
+const int MAX_ITERATIONS = 1'000'000;
+int main() {
+    RdtscpTimer timer;
+    timer.start();
+    for (int i =0; i < MAX_ITERATIONS; i++);
+    timer.finish();
+    cout << "loop cost / iteration " << timer.duration() / (double) MAX_ITERATIONS << endl;
+}
