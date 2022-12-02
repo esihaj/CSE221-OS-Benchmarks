1e83438925f2dec18865fc035e6e46b457f2d496
Ehsan Hajyasini <esihaj@gmail.com>
2022-12-01 22:12:02 -0800

    update requirments.sh

--- requirments.sh
+++ requirments.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo apt install -y htop git curl cpufrequtils curl g++ gcc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
