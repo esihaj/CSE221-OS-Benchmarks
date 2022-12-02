3267b6307d51e3b1045095839aa4c5640e99843a
Ehsan Hajyasini <esihaj@gmail.com>
2022-12-01 22:37:04 -0800

    add rdtscp to loop

--- time/loop_chrono_time.cpp
+++ time/loop_chrono_time.cpp
@@ -1,20 +1,23 @@
 #include <chrono>
 #include <iostream>
 #include <iomanip>
+#include "rdtscp_timer.h"
 using namespace std;
 
 unsigned long long loop_count = 10'000'000;
 unsigned long long warmup_count = 1'000'000;
 
-int main() {
+void chrono_timer() {
     std::cout << std::fixed << std::setprecision(9) << std::left;
     auto start = chrono::high_resolution_clock::now();
     auto end = start;
-        for (auto i = 0ull; i < warmup_count; i++) {
+    for (auto i = 0ull; i < warmup_count; i++)
+    {
         end = chrono::high_resolution_clock::now();
     }
     start = chrono::high_resolution_clock::now();
-    for (auto i = 0ull; i < loop_count; i++) {
+    for (auto i = 0ull; i < loop_count; i++)
+    {
         end = chrono::high_resolution_clock::now();
     }
 
@@ -22,4 +25,29 @@ int main() {
     duration /= loop_count;
     // duration *= 100÷0;
     cout << "time = " << duration.count() << " ns" << endl;
+}
+
+void rdtscp_timer() {
+    std::cout << std::fixed << std::setprecision(9) << std::left;
+    RdtscpTimer timer;
+    
+    for (auto i = 0ull; i < warmup_count; i++)
+    {
+        timer.start();
+    }
+    for (auto i = 0ull; i < loop_count; i++)
+    {
+        timer.finish();
+    }
+
+    unsigned long long duration = timer.duration();
+    duration /= loop_count;
+    cout << "time = " << duration << " cycles" << endl;
+}
+
+int main()
+{
+    chrono_timer();
+    cout << "***********\n";
+    rdtscp_timer();
 }
