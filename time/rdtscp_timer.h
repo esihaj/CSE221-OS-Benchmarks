2fc4241a732d218436d84de81e5d498f63024e38
Ehsan Hajyasini <esihaj@gmail.com>
2022-12-01 22:32:08 -0800

    fix rdtscp

--- time/rdtscp_timer.h
+++ time/rdtscp_timer.h
@@ -10,23 +10,23 @@ private:
 public:
     void start()
     {
-        unsigned int cycles_low, cycles_high,
-            asm volatile("CPUID\n\t"
-                         "RDTSC\n\t"
-                         "mov %%edx, %0\n\t"
-                         "mov %%eax, %1\n\t"
-                         : "=r"(cycles_high), "=r"(cycles_low)::"%rax", "%rbx", "%rcx", "%rdx");
+        unsigned int cycles_low, cycles_high;
+        asm volatile("CPUID\n\t"
+                     "RDTSC\n\t"
+                     "mov %%edx, %0\n\t"
+                     "mov %%eax, %1\n\t"
+                     : "=r"(cycles_high), "=r"(cycles_low)::"%rax", "%rbx", "%rcx", "%rdx");
         start = (((uint64_t)cycles_high << 32) | cycles_low);
     }
 
     void finish()
     {
-        unsigned int cycles_low, cycles_high,
-            asm volatile("RDTSCP\n\t"
-                         "mov %%edx, %0\n\t"
-                         "mov %%eax, %1\n\t"
-                         "CPUID\n\t"
-                         : "=r"(cycles_high), "=r"(cycles_low)::"%rax", "%rbx", "%rcx", "%rdx");
+        unsigned int cycles_low, cycles_high;
+        asm volatile("RDTSCP\n\t"
+                     "mov %%edx, %0\n\t"
+                     "mov %%eax, %1\n\t"
+                     "CPUID\n\t"
+                     : "=r"(cycles_high), "=r"(cycles_low)::"%rax", "%rbx", "%rcx", "%rdx");
         end = (((uint64_t)cycles_high << 32) | cycles_low);
     }
 
