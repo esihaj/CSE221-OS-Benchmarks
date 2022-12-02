864d62e1f2ab0e9d28d195d3cb55b94534a24bef
Ehsan Hajyasini <esihaj@gmail.com>
2022-12-01 23:03:29 -0800

    fix std dev

--- stdev.h
+++ stdev.h
@@ -4,19 +4,12 @@
 #include <numeric>
 #include <cmath>
 #include <iostream>
-const int MAX_ELEMENTS_TO_PRINT 5;
+const int MAX_ELEMENTS_TO_PRINT = 5;
 
 double vec_mean(std::vector<double> const & vec) {
     return std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
 }
-void vec_print(std::vector<double> const & vec) {
-    std::cout << "values = ["
-    for (int i = 0; i < min(MAX_ELEMENTS_TO_PRINT, vec.size()); i++) {
-        std::cout << vec[i] <<", ";
-    }
-    std::cout << "...]\n";
-    std::cout << "mean: " << vec_mean(vec) << " +- " << vec_stddev(vec) << "\n";
-}
+
 double vec_stddev(std::vector<double> const & vec)
 {
     double mean = vec_mean(vec);
@@ -24,6 +17,15 @@ double vec_stddev(std::vector<double> const & vec)
         [](double const & x, double const & y) { return x + y; },
         [mean](double const & x, double const & y) { return (x - mean)*(y - mean); });
     return std::sqrt(sq_sum / vec.size());
+}
+
+void vec_print(std::vector<double> const & vec) {
+    std::cout << "values = ["
+    for (int i = 0; i < min(MAX_ELEMENTS_TO_PRINT, vec.size()); i++) {
+        std::cout << vec[i] <<", ";
+    }
+    std::cout << "...]\n";
+    std::cout << "mean: " << vec_mean(vec) << " +- " << vec_stddev(vec) << "\n";
 }
 
 #endif
