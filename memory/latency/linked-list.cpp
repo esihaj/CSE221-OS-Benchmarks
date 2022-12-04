884c31dc4a316c55a66a4d5c3b5d0f709110b465
Ehsan Hajyasini <esihaj@gmail.com>
2022-12-03 18:22:31 -0800

    fix

--- memory/latency/linked-list.cpp
+++ memory/latency/linked-list.cpp
@@ -6,92 +6,112 @@
 #include <chrono>
 #include <algorithm>
 #include "../../time/rdtscp_timer.h"
+#include "../../hdr.h"
 
 using namespace std;
 
 const int MIN_ACCESS_COUNT = 100'000;
 
-class Node {
-    public:
-    Node* next;
+class Node
+{
+public:
+    Node *next;
     int data;
 };
 
-class LinkedList {
-    private:
-    Node* head;
+class LinkedList
+{
+private:
+    Node *head;
     size_t size;
     RdtscpTimer timer;
-    public:
 
-    LinkedList(int size) : size(size) {
-        Node* contiguous = new Node[size];
+public:
+    LinkedList(int size) : size(size)
+    {
+        Node *contiguous = new Node[size];
         assert(contiguous != nullptr);
 
         head = &(contiguous[0]);
-        Node * current = head;
-
-        long counter = 0;
-        
-        auto rl = random_list(size);
-        cout << "random indexes: size:" << rl.size() <<" indx: ";
-        for (auto index: rl) {
-            if (counter < 10) 
-                cout << index << ", ";
-            if (counter == 10) 
-                cout << "\n";
-            counter++;
+        Node *current = head;
+        for (auto index : random_list(size))
+        {
             current->next = &(contiguous[index]);
             current = current->next;
         }
         current->next = nullptr;
     }
 
-    ~LinkedList() {
+    ~LinkedList()
+    {
         delete[] head;
     }
 
-    vector<size_t> random_list(int max) {
-        vector<size_t> values(max-1);
+    vector<size_t> random_list(int max)
+    {
+        vector<size_t> values(max - 1);
         iota(std::begin(values), std::end(values), 1);
         shuffle(std::begin(values), std::end(values), std::random_device());
         return values;
     }
 
-    long traverse(int iterations) {
-        Node * node;
-        long count = 0, counter = 0;
-        for(int i = 0; i < iterations; i++) {
+    long traverse(int iterations)
+    {
+        Node *node;
+        long count = 0;
+        for (int i = 0; i < iterations; i++)
+        {
             node = head;
-            Node * prev;
-            counter = 0;
-            while (node) {
+            Node *prev;
+            while (node)
+            {
                 prev = node;
                 node = node->next;
-                counter++;
             }
             count += prev->data;
         }
-        cout << "counter " << counter << endl;
         return count;
     }
 
-    void measure(int iterations) {
+    long traverse_once()
+    {
+        Node *node = head;
+        Node *prev;
+        while (node)
+        {
+            prev = node;
+            node = node->next;
+        }
+        return prev->data;
+    }
+
+    void measure(int iterations)
+    {
+        vector<double> measurements;
+        measurements.reserve(iterations);
         auto garbage = traverse(1); // load into cache
-        timer.start();
-        garbage += traverse(iterations);
-        timer.finish();
+        for (int i = 0; i < iterations; i++)
+        {
+            timer.start();
+            garbage += traverse(iterations);
+            timer.finish();
+            measurements.push_back(timer.duration() / (double)(size * iterations));
+        }
         cerr << "garbage: " << garbage << "\n";
-        cout << size * sizeof(Node) / 1024.0 << " KiB -> Cycles:" << timer.duration() / (double)(size * sizeof(Node) * iterations) << " iterations: " << iterations << "\n";
+        cout << size * sizeof(Node) / 1024.0 << " KiB. Iterations: " << iterations << ". cycles/element:" << "\n";
+        print_hdr(measurements);
     }
 };
 
-int find_min_iterations(int size) {
-    return max(10.0, MIN_ACCESS_COUNT / (double) size);
+int find_min_iterations(int size)
+{
+    return max(10.0, MIN_ACCESS_COUNT / (double)size);
 }
 
-int main() {
-    for (int i = 2; i < 1024*1024*1024; i *= 2) {
+int main()
+{
+    for (int i = 2; i < 1024 * 1024 * 1024; i *= 2)
+    {
         LinkedList list(i);
         list.measure(find_min_iterations(i));
     }
