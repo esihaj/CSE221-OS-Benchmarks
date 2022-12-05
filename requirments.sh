9c9bf8a40d73c1f54d4b06bbeda435655f743d0f
Ehsan Hajyasini <esihaj@gmail.com>
2022-12-04 16:52:00 -0800

    add styling to reqs.sh

--- requirments.sh
+++ requirments.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+GREEN='\033[1;32m\n\n\n'
+NC='\033[0m' # No Color
+CURRENT_DIR=$(PWD)
+echo -e "${GREEN}current dir: $CURRENT_DIR ${NC}"
+
 sudo apt update
 sudo apt install -y htop git curl cpufrequtils wget curl g++ gcc tuned msr-tools zlib1g-dev cmake
-
-
-CURRENT_DIR = $(PWD)
-echo "setting up Hdr Histograms"
+echo -e "${GREEN}setting up Hdr Histograms${NC}"
 cd ../
 wget "https://github.com/HdrHistogram/HdrHistogram_c/archive/refs/tags/0.11.6.zip"
 unzip 0.11.6.zip
@@ -17,25 +20,27 @@ sudo ldconfig #https://stackoverflow.com/a/42047062/730033
 echo "LD_LIBRARY_PATH: ["
 echo $LD_LIBRARY_PATH
 echo "]"
-echo "if it is empty run `LD_LIBRARY_PATH=/usr/local/lib`"
+echo "${GREEN}if it is empty run `LD_LIBRARY_PATH=/usr/local/lib`${NC}"
 
 
 cd $CURRENT_DIR
 #disable hyperthreading
-echo "\n\n\n\nhyper threading:"
+echo -n "${GREEN}hyper threading:${NC}"
 sudo echo off > /sys/devices/system/cpu/smt/control
 cat /sys/devices/system/cpu/smt/active
+echo -e "${GREEN} tuned latency-performance profile${NC}"
 #https://github.com/redhat-performance/tuned/
 sudo service tuned start
 sudo service tuned status
 sudo tuned-adm profile latency-performance
 sudo modprobe msr
+echo -e "${GREEN}disable turbo boost${NC}"
 sudo ./turbo-boost.sh disable
 cpufreq-info | head -n 10
 
 
 
-
+echo -e "${GREEN} zsh${NC}"
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
